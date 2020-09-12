b7c9e38085e10f14275d3c9b19458f9dec035196
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-09-12 20:27:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE!>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Wireframe </title>
+<style>
+body {
+	background-image: url("background.jpg");
+
+}
+
+img {
+  border-radius: 50%;
+ 
+  
+}
+p.one {
+  border-style: solid;
+  border-width: 5px;
+  height: 50px;
+  width: 60%;
+  text-align: center;
+  margin: auto;
+  margin-bottom: 250px;
+   font-family: Arial, Helvetica, sans-serif;
+   font-size: 150%;
+  }
+ p.two {
+	border-style: solid;
+	border-width: 7px;
+	height: 250px;
+	width: 99%;
+	text-align: center;
+	font-family: Arial, Helvetica, sans-serif;
+   font-size: 150%;
+	
+}
+
+	
+	
+
+
+  
+
+
+	
+  
+</style>
+</head>
+<body>
+<img src="school.jpg" alt="school" style="width:200px">
+<p class="one">Sliding Banner</p>
+<p class="two">Student/Course</p>
+
+
+ 
+<br>
+</body>
+</html>
